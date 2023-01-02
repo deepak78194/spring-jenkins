dc19020ec4d8a9ad66d2ff94e48ac82202c7b0c3
Deepak B <deepak78194@gmail.com>
2023-01-02 18:14:45 +0530

    second commit

--- src/main/java/com/main/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/main/springjenkins/SpringJenkinsApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringJenkinsApplication {
 
+	public void Hello {
+
+		System.out.println("Hi");
+
+}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
