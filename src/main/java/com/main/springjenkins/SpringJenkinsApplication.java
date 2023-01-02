825477fcd08de42b86ae39434452f8c244b9e92a
Deepak B <deepak78194@gmail.com>
2023-01-02 18:18:15 +0530

    remove error

--- src/main/java/com/main/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/main/springjenkins/SpringJenkinsApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringJenkinsApplication {
 
-	public void Hello {
-
-		System.out.println("Hi");
-
-}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
